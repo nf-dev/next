e32f32db1da103269c47b73d9489075ce4b462bd
Nikita Fomichev <nickita.fomichev2016@yandex.ru>
2025-11-16 19:52:33 +0300

    change git ignore

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,19 @@
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlagin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
+
+export function buildPlugins({
+	paths,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+	return [
+		new HtmlWebpackPlugin({
+			template: paths.html,
+		}),
+		new webpack.ProgressPlugin(),
+		new MiniCssExtractPlagin({
+			filename: "css/[name].[contenthash:8].css",
+			chunkFilename: "css/[name].[contenthash:8].css",
+		}),
+	];
+}
